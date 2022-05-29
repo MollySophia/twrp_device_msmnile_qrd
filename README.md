10fd34df6d6367b243963bf2b86262c4d2d89d05
MollySophia <mollysophia379@gmail.com>
2022-05-29 10:42:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# android_device_qualcomm_qrd855-TWRP
+This is device tree for qualcomm sm8150 QRD
+It's work fine on V2 device
+If u want to build TWRP by yourself just put in device/qualcomm/msmnile
---
+++ twrp_msmnile.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from msmnile device
+$(call inherit-product, device/qualcomm/msmnile/device.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := msmnile
+PRODUCT_NAME := twrp_msmnile
+PRODUCT_BRAND := qti
+PRODUCT_MODEL := msmnile for arm64
+PRODUCT_MANUFACTURER := qualcomm
+PRODUCT_RELEASE_NAME := qti msmnile for arm64
